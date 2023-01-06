abd2d04d18e3eabf8a40210776d0e8b91c7b4122
Md. Sabbir Ahmmed <engsabbir.dev@gmail.com>
2023-01-07 01:59:36 +0600

    footer update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div>
-            <h4>Copyright 2023 by Md. Sabbir Ahmmed. All Rights Reserved.</h4>
+            <h4>Copyright 2021-2023 by Md. Sabbir Ahmmed. All Rights Reserved.</h4>
             <h5>This Website create by The Web Station.</h5>
         </div>
     );
